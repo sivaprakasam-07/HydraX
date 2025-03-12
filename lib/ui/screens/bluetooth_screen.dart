b94540e8fb9decb97a3ad56d73725e14ceb1e49b
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-03-12 07:25:23 +0530

    added bluetooth connection and tried to fix the code

--- lib/ui/screens/bluetooth_screen.dart
+++ lib/ui/screens/bluetooth_screen.dart
@@ -1,67 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-class BluetoothScreen extends StatefulWidget {
-  @override
-  _BluetoothScreenState createState() => _BluetoothScreenState();
-}
+// class BluetoothScreen extends StatefulWidget {
+//   @override
+//   _BluetoothScreenState createState() => _BluetoothScreenState();
+// }
 
-class _BluetoothScreenState extends State<BluetoothScreen> {
-  FlutterBluePlus flutterBlue = FlutterBluePlus();
-  List<BluetoothDevice> devicesList = [];
+// class _BluetoothScreenState extends State<BluetoothScreen> {
+//   FlutterBluePlus flutterBlue = FlutterBluePlus();
+//   List<BluetoothDevice> devicesList = [];
 
-  @override
-  void initState() {
-    super.initState();
-    scanForDevices();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     scanForDevices();
+//   }
 
-  void scanForDevices() {
-    flutterBlue.startScan(timeout: Duration(seconds: 5));
+//   void scanForDevices() {
+//     flutterBlue.startScan(timeout: Duration(seconds: 5));
 
-    flutterBlue.scanResults.listen((results) {
-      for (ScanResult result in results) {
-        if (!devicesList.contains(result.device)) {
-          setState(() {
-            devicesList.add(result.device);
-          });
-        }
-      }
-    });
-  }
+//     flutterBlue.scanResults.listen((results) {
+//       for (ScanResult result in results) {
+//         if (!devicesList.contains(result.device)) {
+//           setState(() {
+//             devicesList.add(result.device);
+//           });
+//         }
+//       }
+//     });
+//   }
 
-  void connectToDevice(BluetoothDevice device) async {
-    await device.connect();
-    Navigator.pop(context, device);
-  }
+//   void connectToDevice(BluetoothDevice device) async {
+//     await device.connect();
+//     Navigator.pop(context, device);
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("Select a Device"),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: devicesList.isEmpty
-          ? Center(child: CircularProgressIndicator(color: Colors.white))
-          : ListView.builder(
-              itemCount: devicesList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    devicesList[index].name.isNotEmpty
-                        ? devicesList[index].name
-                        : "Unknown Device",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  subtitle: Text(devicesList[index].id.toString(),
-                      style: TextStyle(color: Colors.white70)),
-                  trailing: Icon(Icons.bluetooth, color: Colors.blueAccent),
-                  onTap: () => connectToDevice(devicesList[index]),
-                );
-              },
-            ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       appBar: AppBar(
+//         title: Text("Select a Device"),
+//         backgroundColor: Colors.blueAccent,
+//       ),
+//       body: devicesList.isEmpty
+//           ? Center(child: CircularProgressIndicator(color: Colors.white))
+//           : ListView.builder(
+//               itemCount: devicesList.length,
+//               itemBuilder: (context, index) {
+//                 return ListTile(
+//                   title: Text(
+//                     devicesList[index].name.isNotEmpty
+//                         ? devicesList[index].name
+//                         : "Unknown Device",
+//                     style: TextStyle(color: Colors.white),
+//                   ),
+//                   subtitle: Text(devicesList[index].id.toString(),
+//                       style: TextStyle(color: Colors.white70)),
+//                   trailing: Icon(Icons.bluetooth, color: Colors.blueAccent),
+//                   onTap: () => connectToDevice(devicesList[index]),
+//                 );
+//               },
+//             ),
+//     );
+//   }
+// }
