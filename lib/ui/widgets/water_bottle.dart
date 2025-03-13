1480b9d255e304e6ce481351886b456f7aabb907
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-03-13 17:04:37 +0530

    created new branch

--- lib/ui/widgets/water_bottle.dart
+++ lib/ui/widgets/water_bottle.dart
@@ -1,35 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class WaterBottle extends StatelessWidget {
-//   final double fillPercentage; // Water fill level (0 to 1)
-
-//   WaterBottle({required this.fillPercentage});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: 80,
-//       height: 200,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(10),
-//         border: Border.all(color: Colors.blue, width: 2),
-//       ),
-//       child: Stack(
-//         alignment: Alignment.bottomCenter,
-//         children: [
-//           Container(
-//             width: double.infinity,
-//             height: 200 * fillPercentage, // Adjust height based on intake
-//             decoration: BoxDecoration(
-//               color: Colors.blue.withOpacity(0.6),
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class WaterBottle extends StatelessWidget {
